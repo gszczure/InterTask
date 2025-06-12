b89ef12dd8be68ec2586208750f6e4c29c498479
gszczure <grzegorz.sz1202@outlook.com>
2025-06-12 19:18:13 +0200

    Initial commit

---
+++ src/main/java/com/charity/intertask/InterTaskApplication.java
@@ -0,0 +1,13 @@
+package com.charity.intertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterTaskApplication.class, args);
+    }
+
+}
